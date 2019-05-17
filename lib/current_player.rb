0ea3296b6f00eab41cb847ea6a01a9674c8a8c3f
Nichole Battleday <battleday@gmail.com>
2019-05-17 03:37:52 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,9 @@
-turn_count = [board]
-counter = 0
-if turn_count = 
-  counter += 1 
+def turn_count(board)
+  turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      turns += 1
+    end
+  end
+  return turns
 end
